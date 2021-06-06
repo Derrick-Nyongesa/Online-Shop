3ba20c390923ec124df52bc6cdd350a80646cbcd
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-06-06 15:03:03 +0300

    Created a development database, installed dependency and migrated

--- onlineshop/settings.py
+++ onlineshop/settings.py
@@ -83,8 +83,10 @@ WSGI_APPLICATION = 'onlineshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'shop',
+        'USER': 'derrick',
+    	'PASSWORD':'daniel',
     }
 }
 
@@ -113,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 

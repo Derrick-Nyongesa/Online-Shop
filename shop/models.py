6df97a2130b0570f23991187be7e6f805bdc1b5d
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-06-06 15:32:22 +0300

    Created Profile model

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    #name = models.CharField(blank=True, max_length=120)
+    profile_picture = CloudinaryField('image',null=True, default='default.png')
+    location = models.CharField(max_length=100, blank=True)
+    phone_number = models.IntegerField(null=False)
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
